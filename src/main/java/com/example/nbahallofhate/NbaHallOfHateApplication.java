6035462b178e971e3a933b14b83ee4a1e82904c7
Bartek Bober <bartek.bober.koder@gmail.com>
2020-02-20 19:44:28 +0100

    initialize project

---
+++ src/main/java/com/example/nbahallofhate/NbaHallOfHateApplication.java
@@ -0,0 +1,13 @@
+package com.example.nbahallofhate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NbaHallOfHateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NbaHallOfHateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/nbahallofhate/NbaHallOfHateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nbahallofhate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NbaHallOfHateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
