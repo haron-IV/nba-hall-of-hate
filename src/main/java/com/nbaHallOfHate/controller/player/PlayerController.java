b1328f9ad28f2a72110a221d212b956cfb6cbc19
Bartek <bartek.bober.koder@gmail.com>
2020-03-12 12:53:54 +0100

    make packages, add dto for test

---
+++ src/main/java/com/nbaHallOfHate/controller/player/PlayerController.java
@@ -0,0 +1,4 @@
+package com.nbaHallOfHate.controller.player;
+
+public class PlayerController {
+}
---
+++ src/main/java/com/nbaHallOfHate/dto/PlayerDto.java
@@ -0,0 +1,13 @@
+package com.nbaHallOfHate.dto;
+
+public class PlayerDto {
+    private String status = "Active";
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
