ab400dec86b0166801592d74012983bc7c430535
Bartek <bartek.bober.koder@gmail.com>
2020-03-12 13:40:07 +0100

    add 2 working endpoints

--- src/main/java/com/nbaHallOfHate/NbaHallOfHateApi.java
+++ src/main/java/com/nbaHallOfHate/NbaHallOfHateApi.java
@@ -3,10 +3,12 @@ package com.nbaHallOfHate;
 import com.nbaHallOfHate.dto.PlayerDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/players")
@@ -17,12 +19,20 @@ public class NbaHallOfHateApi {
         players = new ArrayList<>();
 
         for (int i = 0; i < 5; i++) {
-            players.add(new PlayerDto("Active", 0));
+            players.add(new PlayerDto(i, "Active"));
         }
     }
 
     @GetMapping("/all")
     public List<PlayerDto> getAllPlayers() {
         return players;
+    }
+
+    @GetMapping
+    public PlayerDto getPlayerById(@RequestParam int id) {
+        Optional<PlayerDto> player = players.stream()
+                .filter( el -> el.getId() == id ).findFirst();
+
+        return player.get();
     }
 }
