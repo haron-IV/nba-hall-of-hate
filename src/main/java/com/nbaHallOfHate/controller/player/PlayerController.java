38223b3819747c59e46d045e7902be4fa1d4b6c1
Bartek <bartek.bober.koder@gmail.com>
2020-03-12 14:31:29 +0100

    add method for adding player

--- src/main/java/com/nbaHallOfHate/controller/player/PlayerController.java
+++ src/main/java/com/nbaHallOfHate/controller/player/PlayerController.java
@@ -1,10 +1,7 @@
 package com.nbaHallOfHate.controller.player;
 
 import com.nbaHallOfHate.dto.PlayerDto;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -33,5 +30,10 @@ public class PlayerController {
         Optional<PlayerDto> player = players.stream().filter( el -> el.getId() == id ).findFirst();
 
         return player.get();
+    }
+
+    @PostMapping
+    public boolean addPlayer(@RequestBody PlayerDto playerDto) {
+        return players.add(playerDto);
     }
 }
