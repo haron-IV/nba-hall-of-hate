a3bc7131fe966c77001da01217aa7d7fbe8f0e66
Bartek <bartek.bober.koder@gmail.com>
2020-03-12 14:14:40 +0100

    add log config and player controllers for test

--- src/main/java/com/nbaHallOfHate/controller/player/PlayerController.java
+++ src/main/java/com/nbaHallOfHate/controller/player/PlayerController.java
@@ -1,4 +1,38 @@
 package com.nbaHallOfHate.controller.player;
 
+import com.nbaHallOfHate.dto.PlayerDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/players")
 public class PlayerController {
+    private List<PlayerDto> players;
+
+    public PlayerController() {
+        players = new ArrayList<>();
+
+        for (int i = 0; i < 5; i++) {
+            players.add(new PlayerDto(i, "Active"));
+        }
+    }
+
+    @GetMapping("/all")
+    public List<PlayerDto> getAllPlayers() {
+        return players;
+    }
+
+    @GetMapping
+    public PlayerDto getPlayerById(@RequestParam int id) {
+        Optional<PlayerDto> player = players.stream()
+                .filter( el -> el.getId() == id ).findFirst();
+//        System.out.println("ROOOROROROOROROROROSOSOOROTORORO");
+        return player.get();
+    }
 }
