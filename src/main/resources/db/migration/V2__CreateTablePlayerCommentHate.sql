aaf2bc71e48d786e3fee3cd27b3f67700dd77c9a
Bartek <bartek.bober.koder@gmail.com>
2020-03-17 00:56:21 +0100

    add autoincrement comment id

--- src/main/resources/db/migration/V2__CreateTablePlayerCommentHate.sql
+++ src/main/resources/db/migration/V2__CreateTablePlayerCommentHate.sql
@@ -1,13 +1,12 @@
 CREATE TABLE public.player_comment_hate
 (
-    comment_id bigint NOT NULL,
+    comment_id SERIAL PRIMARY KEY,
     player_id bigint NOT NULL,
     author character varying NOT NULL,
     created_date date,
     content character varying(500),
     comment_like integer,
     comment_dislike integer,
-    CONSTRAINT comment_id_pk PRIMARY KEY (comment_id),
     CONSTRAINT player_id_fk FOREIGN KEY (player_id)
     REFERENCES public.player (player_id) MATCH SIMPLE
         ON UPDATE NO ACTION
