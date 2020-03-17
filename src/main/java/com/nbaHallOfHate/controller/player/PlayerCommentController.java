9d8124b5c892472c1191b1af1bbf03792554b3cd
Bartek <bartek.bober.koder@gmail.com>
2020-03-17 20:05:05 +0100

    refactor to add adding respect comment

--- src/main/java/com/nbaHallOfHate/controller/player/PlayerCommentController.java
+++ src/main/java/com/nbaHallOfHate/controller/player/PlayerCommentController.java
@@ -1,7 +1,9 @@
 package com.nbaHallOfHate.controller.player;
 
 import com.nbaHallOfHate.entity.PlayerCommentsHateEntity;
+import com.nbaHallOfHate.entity.PlayerCommentsRespectEntity;
 import com.nbaHallOfHate.repository.PlayerCommentsHateRepository;
+import com.nbaHallOfHate.repository.PlayerCommentsRespectRepository;
 import com.nbaHallOfHate.service.player.comment.PlayerCommentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,15 +14,22 @@ import org.springframework.web.bind.annotation.*;
 public class PlayerCommentController {
     private PlayerCommentService playerCommentService;
     private PlayerCommentsHateRepository playerCommentsHateRepository;
+    private PlayerCommentsRespectRepository playerCommentsRespectRepository;
 
     @Autowired
-    public PlayerCommentController(PlayerCommentService playerCommentService, PlayerCommentsHateRepository playerCommentsHateRepository) {
+    public PlayerCommentController(PlayerCommentService playerCommentService, PlayerCommentsHateRepository playerCommentsHateRepository, PlayerCommentsRespectRepository playerCommentsRespectRepository) {
         this.playerCommentService = playerCommentService;
         this.playerCommentsHateRepository = playerCommentsHateRepository;
+        this.playerCommentsRespectRepository = playerCommentsRespectRepository;
     }
 
     @PostMapping("/hate")
     public PlayerCommentsHateEntity addHateComment(@RequestBody PlayerCommentsHateEntity playerCommentsHateEntity) throws Exception {
         return playerCommentService.addHateComment(playerCommentsHateEntity);
+    }
+
+    @PostMapping("/respect")
+    public PlayerCommentsRespectEntity addRespectComment(@RequestBody PlayerCommentsRespectEntity playerCommentsRespectEntity) throws Exception {
+        return playerCommentService.addRespectComment(playerCommentsRespectEntity);
     }
 }
--- src/main/java/com/nbaHallOfHate/repository/PlayerCommentRepository.java
+++
@@ -1,8 +0,0 @@
-package com.nbaHallOfHate.repository;
-
-import com.nbaHallOfHate.entity.PlayerCommentsHateEntity;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PlayerCommentRepository extends CrudRepository<PlayerCommentsHateEntity, Long>{}
