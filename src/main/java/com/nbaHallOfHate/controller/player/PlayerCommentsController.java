a7abe48d3de18b8b1d2dbec63e7a7d919f2c31d3
Bartek <bartek.bober.koder@gmail.com>
2020-03-17 19:00:47 +0100

    final add enpoint

--- src/main/java/com/nbaHallOfHate/controller/player/PlayerCommentsController.java
+++ src/main/java/com/nbaHallOfHate/controller/player/PlayerCommentsController.java
@@ -1,12 +1,12 @@
 package com.nbaHallOfHate.controller.player;
 
 import com.nbaHallOfHate.entity.PlayerCommentsHateEntity;
+import com.nbaHallOfHate.entity.PlayerCommentsRespectEntity;
 import com.nbaHallOfHate.repository.PlayerCommentsHateRepository;
+import com.nbaHallOfHate.repository.PlayerCommentsRespectRepository;
 import com.nbaHallOfHate.service.player.comment.PlayerCommentsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Set;
 
 @RestController
 @RequestMapping("/api/player-comments")
@@ -14,11 +14,13 @@ import java.util.Set;
 public class PlayerCommentsController {
     private PlayerCommentsService playerCommentsService;
     private PlayerCommentsHateRepository playerCommentsHateRepository;
+    private PlayerCommentsRespectRepository playerCommentsRespectRepository;
 
     @Autowired
-    public PlayerCommentsController(PlayerCommentsService playerCommentsService, PlayerCommentsHateRepository playerCommentsHateRepository) {
+    public PlayerCommentsController(PlayerCommentsService playerCommentsService, PlayerCommentsHateRepository playerCommentsHateRepository, PlayerCommentsRespectRepository playerCommentsRespectRepository) {
         this.playerCommentsService = playerCommentsService;
         this.playerCommentsHateRepository = playerCommentsHateRepository;
+        this.playerCommentsRespectRepository = playerCommentsRespectRepository;
     }
 
     @GetMapping("/hate")
@@ -29,6 +31,11 @@ public class PlayerCommentsController {
     @GetMapping("/hate/{id}")
     public Iterable<PlayerCommentsHateEntity> getAllHateComments(@PathVariable Long id) {
         return playerCommentsHateRepository.findAllHateCommentByPlayerId(id);
+    }
+
+    @GetMapping("/respect/{id}")
+    public Iterable<PlayerCommentsRespectEntity> getAllRespectComments(@PathVariable Long id) {
+        return playerCommentsRespectRepository.findAllRespectCommentByPlayerId(id);
     }
 }
 
